5b3bdba89da328567d416d8ad243b56000a8c266
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-04-19 13:41:54 -0300

    feat: add and remove todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { TodosComponent } from './components/todos/todos.component';
     TodosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -9,6 +9,8 @@ import { Todo } from '../../models/Todo';
 export class TodosComponent implements OnInit {
 
   todos:Todo[];
+
+  inputTodo:string = "";
   
   constructor() { }
 
@@ -23,6 +25,27 @@ export class TodosComponent implements OnInit {
         completed: true
       }
     ]
+  }
+
+  toggleDone(id:number) {
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed
+
+      return v;
+    })
+  }
+
+  deleteTodo(id:number) {
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  addTodo () {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    });
+
+    this.inputTodo = "";
   }
 
 }
